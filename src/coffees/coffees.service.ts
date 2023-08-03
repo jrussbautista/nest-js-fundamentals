feb4f97319ed16138403d95a5ae17591fcf974e1
James Russel C. Bautista <jrussclay@gmail.com>
2023-08-03 17:35:52 +0800

    Add coffees features

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Coffee 1',
+      brand: 'Coffee brand 1',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: number) {
+    const coffee = this.coffees.find((coffee) => coffee.id === id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee ${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    return this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // update existing coffee
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((coffee) => coffee.id === +id);
+    if (coffeeIndex !== -1) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
