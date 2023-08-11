fe345834e8f5dd69af6bdae510600103e2d239c9
James Russel C. Bautista <jrussclay@gmail.com>
2023-08-11 15:37:01 +0800

    Updates on setting up swagger

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -16,11 +16,14 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ description: 'Forbidden' })
   @Public()
   @Get()
   async findAll(
