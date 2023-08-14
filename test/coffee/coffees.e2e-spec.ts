145bcbc32506f3b08b37e8809dacf146e341f448
James Russel C. Bautista <jrussclay@gmail.com>
2023-08-14 13:26:30 +0800

    Add test database and setup e2e for coffees feature

---
+++ test/coffee/coffees.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+
+describe('[Feature] Coffees - /coffees', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+          host: 'localhost',
+          port: 5433,
+          type: 'postgres',
+          username: 'postgres',
+          password: 'pass123',
+          database: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it.todo('Create [POST /]');
+  it.todo('Get all [GET /]');
+  it.todo('Get one [GET /:id]');
+  it.todo('Update one [PATCH /:id]');
+  it.todo('Delete one [DELETE /:id]');
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
